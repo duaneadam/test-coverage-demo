80b2eed17050ccc71bc5047d4a35c4238ddf1d8b
Duane Adam <hello@duaneadam.me>
2021-01-11 12:51:10 +0800

    test pass

--- apps/mindvalley_com/assets/quest/tests/unit/example.spec.js
+++ apps/mindvalley_com/assets/quest/tests/unit/example.spec.js
@@ -9,12 +9,4 @@ describe('HelloWorld.vue', () => {
     });
     expect(wrapper.text()).toMatch(msg);
   });
-
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch('fail example');
-  });
 });
