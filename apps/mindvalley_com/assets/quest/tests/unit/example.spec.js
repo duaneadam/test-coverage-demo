63f9aa8f3f8d202a8e6e5fc34cf472a7d9bcb50f
Duane Adam <hello@duaneadam.me>
2021-01-12 21:14:25 +0800

    Passing test

--- apps/mindvalley_com/assets/quest/tests/unit/example.spec.js
+++ apps/mindvalley_com/assets/quest/tests/unit/example.spec.js
@@ -7,6 +7,6 @@ describe('HelloWorld.vue', () => {
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
-    expect(wrapper.text()).toMatch('Hello World');
+    expect(wrapper.text()).toMatch('Hello Jest');
   });
 });
